71ba9625a4ea0af06fa2c1676fc7e73e1cbf3fe2
ayushbharagava <57057106+ayushbharagava@users.noreply.github.com>
2020-06-06 12:48:36 +0530

    sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { OffersComponent } from './offers/offers.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accomodation', component: AccomodationComponent},
+  { path: 'celebrations', component: CelebrationsComponent},
+  { path: 'food-beverage', component: FoodBeverageComponent},
+  { path: 'lifestyle', component: LifestyleComponent},
+  { path: 'weddings', component: WeddingsComponent},
+  { path: 'offers', component: OffersComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
-    imports: [MatBadgeModule,MatButtonModule],
-    exports: [MatBadgeModule,MatButtonModule]
+    imports: [MatBadgeModule,MatButtonModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule],
+    exports: [MatBadgeModule,MatButtonModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule]
 })
 
 export class MaterialModule {}
