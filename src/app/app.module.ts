9aa43b53db57e5ab8f0eb6b20f588fa2bcd9eb38
ayushbharagava <57057106+ayushbharagava@users.noreply.github.com>
2020-06-06 18:46:16 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule} from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -58,7 +59,8 @@ import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,11 +5,12 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
-    imports: [MatBadgeModule,MatButtonModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule],
-    exports: [MatBadgeModule,MatButtonModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule]
+    imports: [MatBadgeModule,MatButtonModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,MatTabsModule],
+    exports: [MatBadgeModule,MatButtonModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,MatTabsModule]
 })
 
 export class MaterialModule {}
