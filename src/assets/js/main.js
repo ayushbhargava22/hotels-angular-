81ae5b574c2dbf22d67fa8659787ca826438c0f2
ayushbharagava <57057106+ayushbharagava@users.noreply.github.com>
2020-06-08 12:54:03 +0530

    shoehideupdated

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,17 +9,21 @@ $('.advantage-wrapper').slick({
   });
   document.getElementById("showhidebtn").addEventListener("click", function() {
     var showhide1 = document.getElementById("moreinfo-showhide")
+    var btn1 = document.getElementById("showhidebtn")
     if(showhide1.style.display == "none")
     {
         showhide1.style.display = "block"
+        btn1.style.display = "none"
     }
 })
 
 
 document.getElementById("hide-button").addEventListener("click", function() {
   var hide1 = document.getElementById("moreinfo-showhide")
-  if(hide1.style.display == "block")
+  var btn1 = document.getElementById("showhidebtn")
+  if(hide1.style.display == "block" && btn1.style.display == "none")
   {
       hide1.style.display = "none"
+      btn1.style.display = "flex"
   }
 })
