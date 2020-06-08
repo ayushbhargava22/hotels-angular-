a006f802e56f23f5937ac2f36ead008bf99f90f1
ayushbharagava <57057106+ayushbharagava@users.noreply.github.com>
2020-06-08 19:18:16 +0530

    Auto-generated commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+$('.advantage-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+  });
+  document.getElementById("showhidebtn").addEventListener("click", function() {
+    var showhide1 = document.getElementById("moreinfo-showhide")
+    var btn1 = document.getElementById("showhidebtn")
+    if(showhide1.style.display == "none")
+    {
+        showhide1.style.display = "block"
+        btn1.style.display = "none"
+    }
+})
+
+
+document.getElementById("hide-button").addEventListener("click", function() {
+  var hide1 = document.getElementById("moreinfo-showhide")
+  var btn1 = document.getElementById("showhidebtn")
+  if(hide1.style.display == "block" && btn1.style.display == "none")
+  {
+      hide1.style.display = "none"
+      btn1.style.display = "flex"
+  }
+})
